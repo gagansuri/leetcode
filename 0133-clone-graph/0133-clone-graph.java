e1be6fcbbec8382f7fc1d2532e8b72ebfff482fc
GAGAN SURI <gagan.suri@gmail.com>
2025-09-24 10:24:29 -0500

    Time: 24 ms (84.74%), Space: 43.2 MB (12.71%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,72 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+
+        // clone the node and go over the list of nodes
+        // mapping of node in original and in the clone 
+        if (node == null)
+            return null;
+        Map<Node, Node> lookup = new HashMap<>();
+        return cloneGraph(node, lookup);
+    }
+
+    Node cloneGraph(Node node, Map<Node, Node> lookup) {
+        Node copy = new Node(node.val);
+        lookup.put(node, copy);
+        for (Node n : node.neighbors) {
+            // node is already been copied
+            if (lookup.containsKey(n)) {
+                copy.neighbors.add(lookup.get(n));
+            } else {
+                // create a copy deep of the neighbor node
+                Node c = cloneGraph(n, lookup);
+                //lookup.put(n,c);
+                copy.neighbors.add(c);
+            }
+        }
+        return copy;
+    }
+
+    public Node cloneGraph1(Node node) {
+        Map<Node, Node> lookup = new HashMap<>();
+        if (node == null)
+            return node;
+        return traverse(node, lookup);
+    }
+
+    private Node traverse(Node node, Map<Node, Node> lookup) {
+        //create a copy of node
+        Node copy = new Node(node.val);
+        lookup.put(node, copy);
+        for (Node neighbor : node.neighbors) {
+            if (lookup.containsKey(neighbor)) {
+                copy.neighbors.add(lookup.get(neighbor));
+            } else {
+                Node neighborCopy = traverse(neighbor, lookup);
+                copy.neighbors.add(neighborCopy);
+            }
+        }
+
+        return copy;
+
+    }
+}
