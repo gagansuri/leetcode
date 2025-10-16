abc0adc46afdd2464a075df10ca56c08d5ac6be4
GAGAN SURI <gagan.suri@gmail.com>
2025-10-16 08:55:15 -0500

    Time: 0 ms (100%), Space: 41.7 MB (85.11%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-       int[] res = new int[digits.length + 1];
-       int carry = 1 ;
-       for(int i = digits.length - 1 ; i >= 0 ; i--) {
-        int sum = digits[i] + carry ;
-        res[i] = sum % 10;
-        carry = sum / 10;
-       }
-       
-       if(carry > 0 ) {
-        res[0] = carry;
-       } else {
-        res = Arrays.copyOf(res, res.length - 1);
-       }
-       return res;
+      for(int i = digits.length - 1 ; i >= 0 ; i--) {
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+      }
+    
+// all 9
+    /*int[] res = new int[digits.length + 1];
+    res[0] = 1;
+    return res;*/
+
+    int[] result = new int[digits.length + 1];
+    result [0]  = 1;
+    return result;
     }
+
 }
