13c2b4c74c52d72542ef960130ec230283597d67
GAGAN SURI <gagan.suri@gmail.com>
2025-11-06 10:59:58 -0600

    Time: 70 ms (55.52%), Space: 42.9 MB (17.93%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,30 +1,35 @@
 class Solution {
     public int getMaximumGold(int[][] grid) {
-        // iterate over each element in the grid and check if the value is not zero
-        int max = 0 ;
-        for(int i = 0 ; i < grid.length ; i++) {
-            for(int j = 0 ; j < grid[0].length ; j++) {
-                if(grid[i][j] != 0) {
-                    int gold = findGoldFromPath(grid, i , j, new boolean[grid.length][grid[0].length]);
-                    max = Math.max(max, gold);
-                }
-         }
+    if(grid == null || grid.length == 0) return 0;
+    int rows = grid.length, columns = grid[0].length;
+    boolean[][] seen = new boolean[rows][columns];
+    int max = 0;
+
+    // only start the search from elements which dont have a zero
+    for(int i = 0 ; i < rows; i ++) {
+        for(int j = 0 ; j < columns; j++) {
+            if(grid[i][j] != 0) {
+                int gold = dfs(grid, i , j, seen);
+                max = Math.max(gold,max);
+            }
         }
-        return max;
+    }
+    return max;
     }
 
-    int findGoldFromPath(int[][]grid, int row , int col, boolean[][] visited) {
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length ||
-            grid[row][col] == 0 || visited[row][col] == true) {
-                return 0;
-            }
-        visited[row][col] = true;
-        int up      =  findGoldFromPath(grid, row - 1 , col, visited); //up
-        int down    =  findGoldFromPath(grid, row + 1 , col, visited); // down
-        int left    =  findGoldFromPath(grid, row , col - 1, visited);// left
-        int right   =  findGoldFromPath(grid, row , col + 1, visited);//right
-        visited[row][col] = false;
-        int max = Math.max(up, Math.max(down,Math.max(left,right))) +  grid[row][col];
-        return max;      
+
+    int dfs(int[][] grid, int row, int col, boolean[][] seen) {
+        // first check boundaries
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == 0 || seen[row][col] == true ) {
+            return 0;
+        }
+        seen[row][col] = true;
+        int left = dfs(grid, row, col - 1, seen);
+        int right = dfs(grid, row, col + 1, seen);
+        int up = dfs(grid, row - 1, col, seen);
+        int down = dfs(grid, row +1, col, seen);
+        seen[row][col] = false;
+        return Math.max(left, Math.max(right, Math.max(up,down))) + grid[row][col];
+
     }
 }
