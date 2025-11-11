a35b4c9ddc3af6915e293357c23f0b0721079a3d
GAGAN SURI <gagan.suri@gmail.com>
2025-11-10 23:02:19 -0600

    Time: 3 ms (84.63%), Space: 52.6 MB (10.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,156 +1,34 @@
 class Solution {
   public int numIslands(char[][] grid) {
-    // check all the elements 
-    // when you see a 1 - do a dfs and reach all the neighboring nodes
-    // if the node is 1 set it to zero
-    // all the ones in dfs are counted together as 1 island only
     int count = 0 ;
     for(int i = 0 ; i < grid.length; i++) {
         for(int j = 0 ; j < grid[i].length; j++) {
-            // check if 1
             if(grid[i][j] == '1') {
                 count++;
-                dfs1(grid, i, j);
+                dfs(grid, i , j);
             }
         }
-    }   
+    }
     return count;
   }
+  void dfs(char[][] grid, int row, int col) {
 
-
-    void dfs1(char[][] grid, int row, int col) {
-        // check if within bounds and not ! = 0
-        if(row < 0 || row > grid.length - 1 || col < 0 || col >  grid[row].length - 1  || grid[row][col] == '0' ) {
-            return;
-        }
-    // here row col are valid and equal to 1
+    if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0') {
+        return;
+    }
 
     grid[row][col] = '0';
-    dfs(grid, row - 1 , col);
-    dfs(grid, row , col - 1);
-    dfs(grid, row , col + 1);
-    dfs(grid, row + 1 , col);
-    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public int numIslands1(char[][] grid) {
-        int count = 0 ; 
-        for(int i = 0 ; i < grid.length; i++) {
-            for(int j = 0 ; j < grid[0].length;j++) {
-                //System.out.println(" i: "+i +" j: "+j +" grid[i][j]: "+grid[i][j]);
-                if(grid[i][j] == '1') {
-                    count++;
-                    dfs(grid,i,j);
-                }
-
-            }
-        }
-
-        return count;
-    }
-
-    public void dfs(char[][] grid, int row, int col) {
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[row].length || grid[row][col] == '0') {
-            return;
-        }
-        grid[row][col] = '0';
-        dfs(grid, row+1,col);
-        dfs(grid, row-1,col);
-        dfs(grid, row,col+1);
-        dfs(grid, row,col-1);
-
-    }
-}
+    // all connecting to be counted as with the original and should be set to 0
+    //left
+   dfs(grid, row, col - 1);
+   // right
+   dfs(grid, row, col + 1);
+   //up
+   dfs(grid, row - 1, col);
+   // down
+   dfs(grid, row + 1, col);
+
+
+  }
+  }
