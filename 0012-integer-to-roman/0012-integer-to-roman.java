537c965596e9a086585ad3774e56f83afadc9915
GAGAN SURI <gagan.suri@gmail.com>
2025-11-13 07:58:30 -0600

    Time: 5 ms (24.67%), Space: 46.8 MB (6.21%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,26 +1,19 @@
 class Solution {
     public String intToRoman(int num) {
-    int[] numbers = {1000, 900, 500, 400, 100, 90,50,40, 10 , 9 , 5 , 4 , 1};
-    String[] roman = {"M", "CM", "D", "CD", "C" , "XC","L","XL","X" , "IX" , "V" , "IV","I"};
+    int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9 , 5, 4, 1};
+    String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX" , "V", "IV", "I"};
     StringBuilder result = new StringBuilder();
     for(int i = 0 ; i < numbers.length; i++) {
-        if(num != 0 && num >= numbers[i]) {
-            int n = num / numbers[i];
-           // System.out.println(" n "+ n + " num : "+ num + " numbers[i]: "+numbers[i]);
-            if(n != 0) {
-             result.append(concat(n, roman[i]));
-             num = num % numbers[i];
-            }
-           
-        }
+        int count = num/numbers[i];
+        result.append(concat(count,roman[i]));
+        num = num % numbers[i];
     }
-    return result.toString();
-    
+    return result.toString();    
     }
 
-    private String concat(int n , String s) {
+    private String concat(int count, String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < n ; i++) {
+        for(int i = 0 ; i < count; i++) {
             sb.append(s);
         }
         return sb.toString();
