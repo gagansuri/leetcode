8b720647b1cdf0565d0755be27ca18860f4b2480
GAGAN SURI <gagan.suri@gmail.com>
2025-12-04 12:48:42 -0600

    Time: 157 ms (22.74%), Space: 108.8 MB (63.87%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -0,0 +1,36 @@
+class TimeMap {
+    Map<String,TreeMap<Integer,String>> map;
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)) map.put(key, new TreeMap<>());
+        
+        TreeMap<Integer,String> innerMap = map.get(key);
+        innerMap.put(timestamp, value);
+        map.put(key, innerMap);
+       // System.out.println(" set:" + map);
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)) return "";
+
+       TreeMap<Integer,String> inner = map.get(key);
+        if(inner.containsKey(timestamp)) return inner.get(timestamp);
+ /*
+        System.out.println(" get:" + map + " innermap : "+inner.firstEntry().getValue() + " key : "+ key + " timestamp : "+ timestamp);
+        return inner.firstEntry().getValue().toString();
+*/
+        Map.Entry<Integer,String> entry = inner.floorEntry(timestamp);
+
+        return entry == null ? "" : entry.getValue();
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
