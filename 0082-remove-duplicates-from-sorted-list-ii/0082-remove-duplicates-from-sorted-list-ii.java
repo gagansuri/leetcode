12e4425f934d883e1d8792f13391423ea2f9a56e
GAGAN SURI <gagan.suri@gmail.com>
2026-02-04 14:14:30 -0600

    Time: 0 ms (100%), Space: 44.8 MB (83.26%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,22 +10,23 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-      ListNode dummy = new ListNode();
-      dummy.next = head;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode curr = head;
 
-      ListNode pre = dummy, curr = head;
-      while(curr != null) {
-        if(curr.next != null && curr.val == curr.next.val) {
-            while(curr.next != null && curr.val == curr.next.val) {
-                curr = curr.next; //reached at the end of duplicates curr points to the last duplicate; 
+        while(curr != null) {
+            if(curr.next != null && curr.val == curr.next.val) {
+                // keep the pre and move on curr till curr and next dont match
+                while(curr.next != null && curr.val == curr.next.val) curr = curr.next;
+                pre.next = curr.next;
+                curr = curr.next;
+            } else {
+                pre = curr;
+                curr = curr.next;
             }
-            pre.next = curr.next;
-            curr = curr.next;
-        } else {
-            pre = curr;
-            curr = curr.next;
         }
-      } 
-    return dummy.next;
+        return dummy.next;
+
     }
 }
