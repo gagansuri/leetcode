6325afc5284f52b401ca000caf8727502c5c7ece
GAGAN SURI <gagan.suri@gmail.com>
2026-02-05 17:47:23 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        Set<List<Integer>> res = new HashSet<>();
+        for(int i = 0 ; i < nums.length - 3; i++) {
+            for(int j = i + 1 ; j < nums.length - 2 ; j++) {
+                int left = j + 1, right = nums.length - 1;
+                    while(left < right) {
+                        long sum = nums[i] + nums[j] + nums[left] + nums[right];
+                        if(sum == target) {
+                            res.add(List.of(nums[i], nums[j], nums[left++] , nums[right--]));    
+                        } else if(sum > target) {
+                            right--;
+                        } else {
+                            left++;
+                        }
+                    }
+            }
+        }
+        return new ArrayList<>(res);
+    }
+}
