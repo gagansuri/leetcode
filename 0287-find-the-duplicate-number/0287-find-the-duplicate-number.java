52cb06a1a14aa8978ced757b5bd57090e0ebb12b
GAGAN SURI <gagan.suri@gmail.com>
2026-02-04 20:21:51 -0600

    Time: 2 ms (98.62%), Space: 81.5 MB (86.06%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,15 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        int nums1[] = new int[nums.length+1];
+        for(int i = 0 ; i < nums.length; i++) {
+            if(nums1[nums[i]] == nums[i]) return nums[i];
+
+            nums1[nums[i]] = nums[i];
+        }
+        return -1; 
+    }
+
+    public int findDuplicate1(int[] nums) {
         for(int i = 0 ; i < nums.length; i++) {
             int a = nums[i];
             if(a < 0 ) a = a*-1;
