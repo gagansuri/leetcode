c09cb2c1911b958a7a56ef40795f5a77719f9d8e
GAGAN SURI <gagan.suri@gmail.com>
2026-04-06 12:39:31 -0500

    Time: 0 ms (100.00%) | Memory: 42.8 MB (86.02%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -28,42 +28,5 @@ class Solution {
 
         return root;
     }
-    public TreeNode invertTree1(TreeNode root) {
-        // take swap left and right nodes and continue
-
-        if(root == null) return null;
-
-        TreeNode temp = new TreeNode();
-        temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-        invertTree(root.left);
-        invertTree(root.right);
-
-        return root;
     
-    }
-    public TreeNode dfsTree(TreeNode root) {
-        if(root == null) return root;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i = 0 ; i < size; i++) {
-                TreeNode node = queue.poll();
-                System.out.print(node.val);
-                if(node.left != null) {
-                    queue.add(node.left);
-                }
-                if(node.right != null) {
-                    queue.add(node.right);
-                }
-            }
-
-            System.out.println("");
-
-        }
-        return null;
-    }
 }
