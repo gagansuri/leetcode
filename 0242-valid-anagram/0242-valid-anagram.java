20ab529719442a1275f1ae45b0eac426b8c25e7b
GAGAN SURI <gagan.suri@gmail.com>
2026-06-02 13:54:01 -0500

    Time: 16 ms (22%), Space: 46.6 MB (15.3%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+    // either sort and compare nlogn + n
+
+    // create a frequency map n + n + n O(3n) - O(n)
+    if(s == null && t == null) return true;
+
+    if(s == null || t == null ) return false;
+
+    if(s.length() != t.length()) return false;
+
+    Map<Character, Integer> lookup = new HashMap<>();
+    for(char c : s.toCharArray()) {
+        lookup.put(c, lookup.getOrDefault(c, 0)+1);
+    }
+
+    for(char c : t.toCharArray()) {
+        if(lookup.containsKey(c)) {
+            lookup.put(c, lookup.get(c) - 1);
+            if(lookup.get(c) == 0 ) {
+                lookup.remove(c);
+            }
+        } else {
+            return false;
+        }
+    }
+        return lookup.size() == 0 ? true : false;
+    }
+}
