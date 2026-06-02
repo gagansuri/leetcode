b8ec3d8dc16828c34972d3e1ff1089187541256e
GAGAN SURI <gagan.suri@gmail.com>
2026-06-02 13:43:47 -0500

    Time: 2 ms (99.39%), Space: 47.4 MB (11.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> lookup = new HashMap<>();
+        if(nums == null || nums.length == 0 ) return null;
+        Map<Integer, Integer> lookup = new HashMap<>();
 
-        for(int i = 0 ; i < nums.length; i++) {
+        for(int i = 0 ; i < nums.length ; i++) {
             if(lookup.containsKey(target - nums[i])) {
-                return new int[]{i, lookup.get(target-nums[i])};
+                //match found;
+                int[] result = new int[2];
+                result[0] = lookup.get(target - nums[i]);
+                result[1] = i; 
+                return result;
             } else {
-                lookup.put(nums[i], i);
+                lookup.put(nums[i] ,  i );
             }
         }
-        return null;
+    return null;
     }
 }
